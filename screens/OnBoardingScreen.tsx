50350dca702ccc2ff51059cc37b923f8066da41b
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-04-27 13:23:58 +0100

    project initialized

---
+++ screens/OnBoardingScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+
+type Props = {}
+
+const OnBoardingScreen = (props: Props) => {
+    return(
+        <View>
+        </View>
+    )
+}
+
+export default OnBoardingScreen
